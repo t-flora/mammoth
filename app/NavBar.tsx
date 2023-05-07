12bda04956347e580df0722e180a0ecb6cee8f13
Tiago Flora <tiagomflora@gmail.com>
2023-05-07 15:51:22 -0700

    add navbar component

---
+++ app/NavBar.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './NavBar.module.css';
+
+export default function NavBar() {
+    return (
+        <nav className={styles.nav}>
+            <Link href={'/'}>
+                <Image 
+                    src={'../public/next.svg'}
+                    width={216}
+                    height={30}
+                    alt='Mammoth-Next logo'
+                />
+            </Link>
+            <ul className={styles.links}>
+                <li>
+                    <Link href={'/about'}>about</Link>
+                </li>
+                <li>
+                    <Link href={'/blog'}>blog</Link>
+                </li>
+                <li>
+                    <Link href={'/users'}>users</Link>
+                </li>
+                <li>
+                    {/* Future button */}
+                </li>
+            </ul>
+        </nav>
+    )
+}
