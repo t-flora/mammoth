75c875ba50955dde4ce710af2b9b621aa185720a
Tiago Flora <tiagomflora@gmail.com>
2023-05-09 19:49:43 -0700

    add Prisma ORM scheme and API data fetching

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import type { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { prisma } from '@/lib/prisma';
 
 export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
     secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GithubProvider({
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Image from 'next/image'
 import { Roboto } from 'next/font/google'
-import { useSession } from 'next-auth/react';
-import { redirect } from 'next/navigation';
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -9,10 +7,6 @@ const roboto = Roboto({
 });
 
 export default function Home() {
-  const { data } = useSession();
-  if(!data) {
-    redirect('/api/auth/signin');
-  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
